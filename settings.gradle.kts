7797606e8a2e86788ef2cfd3f42559fdf8573036
Le Thi Ngoc Lan <lnlan1810@gmail.com>
2023-12-07 00:30:46 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("UnstableApiUsage")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Game Characters"
+include(":app")
+include(":core:base")
+include(":core:di")
+include(":core:extensions")
+include(":core:navigation")
+include(":core:network")
+include(":core:ui")
+
+include(":feature:home")
+
+
+
+
+ 
